f0f61e54c86bb7a99550e35228ef85044075366f
RU-CORE-LAB <167742783+RU-CORE-LAB@users.noreply.github.com>
2024-04-29 13:49:40 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,14 +9,23 @@ setup(
     author_email='rucore.center@gmail.com, nick.ratchanan@gmail.com',
     url='https://github.com/RU-CORE-LAB/wNetCDF2DSSAT',
     install_requires=[
-        'cftime>=1.6.3',
-        'h5netcdf>=1.3.0',
-        'geopandas>=0.14.3',
-        'netCDF4>=1.6.5',
-        'pandas>=2.2.2',
-        'pyshp>=2.3.1',
-        'xarray>=2024.3.0',
-        'tqdm>=4.66.2'
+        'cftime',
+        'h5netcdf',
+        'geopandas',
+        'netCDF4',
+        'pandas',
+        'pyshp',
+        'xarray',
+        'tqdm'
+
+        #'cftime>=1.6.3',
+        #'h5netcdf>=1.3.0',
+        #'geopandas>=0.14.3',
+        #'netCDF4>=1.6.5',
+        #'pandas>=2.2.2',
+        #'pyshp>=2.3.1',
+        #'xarray>=2024.3.0',
+        #'tqdm>=4.66.2'
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
