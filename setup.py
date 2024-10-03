a0876aaf7f370e49ab07de09e7cd17d94af7b0d0
RU-CORE-LAB <167742783+RU-CORE-LAB@users.noreply.github.com>
2024-10-03 10:30:09 +0700

    Update setup.py
    numpy

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email='rucore.center@gmail.com, nick.ratchanan@gmail.com',
     url='https://github.com/RU-CORE-LAB/wNetCDF2DSSAT',
     install_requires=[
-        'numpy==1.26.4',
+        'numpy',
         'cftime',
         'h5netcdf',
         'geopandas',
@@ -23,7 +23,7 @@ setup(
         'tqdm',
         'matplotlib',
         'python-cmethods',
-
+        #'numpy==1.26.4',
         #'cftime>=1.6.3',
         #'h5netcdf>=1.3.0',
         #'geopandas>=0.14.3',
